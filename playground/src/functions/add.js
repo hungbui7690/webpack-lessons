100182060b98392909881c13958b4a32e868e388
Hung Bui <hungbui7690@gmail.com>
2023-03-16 15:32:30 -0600

    CommonJS modules P2

--- playground/src/functions/add.js
+++ playground/src/functions/add.js
@@ -2,4 +2,12 @@ function add(a, b) {
   return a + b
 }
 
-module.exports = add
+function addAndMultiply(a, b, c) {
+  return (a + b) * c
+}
+
+// (***) named export
+module.exports = {
+  add: add,
+  addAndMultiply,
+}
--- playground/src/main.js
+++ playground/src/main.js
@@ -1,15 +1,16 @@
 /*
-  CommonJS modules P1
-  > pic: modules
-
-  - create folder functions/add.js  + subtract.js
-    > using module.exports
-  - run: node main.js
+  CommonJS modules P2
+  - export multiple functions in a file > add.js
+    > to export multiple things > use named export
+  
 
 */
 
-const add = require('./functions/add')
 const subtract = require('./functions/subtract')
+
+// (***)
+const { add, addAndMultiply } = require('./functions/add')
 
 console.log(add(2, 3))
 console.log(subtract(2, 3))
+console.log(addAndMultiply(2, 3, 4)) // 20
