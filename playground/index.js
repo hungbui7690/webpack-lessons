826a1c164bae943a13b8d7a3ebfb2f5137ba56af
Hung Bui <hungbui7690@gmail.com>
2023-03-16 11:37:01 -0600

    Basic Setup

---
+++ playground/index.js
@@ -0,0 +1,16 @@
+/*
+  - Intro
+  - Why
+  - How 
+    > pic 
+
+///////////////////////////////////
+
+  Basic HTML Javascript Setup
+
+*/
+
+function buttonClick() {
+  const element = document.querySelector('#header')
+  element.textContent = 'Code is updated!!!'
+}
