f338ed53b001322b9b63d2c3ebeee74138970a82
Hung Bui <hungbui7690@gmail.com>
2023-03-17 09:50:10 -0600

    Multiple Entry Bundle P1: Initial Setup

--- playground/src/index.js
+++ playground/src/index.js
@@ -1,3 +1,43 @@
 /*
+  Intro 
+
+  Multiple Entry Bundle P1
+  - pic
+  - right now, cannot display data from json files on the screen
+
 
 */
+
+import _ from 'lodash'
+import newArrivalData from './newArrival.json'
+
+function renderItems(itemArr) {
+  const cakeContainerEl = document.getElementById('cakesContainer')
+  cakeContainerEl.innerHTML = ``
+  _.forEach(itemArr, (item) => {
+    let cakeEl = document.createElement('div')
+    cakeEl.classList.add('cakes-container')
+    cakeEl.innerHTML = `
+    <img src="./assets/${item.imgName}" />
+    <strong>${item.title}</strong>
+    <span>${item.price}</span>
+    `
+    cakeContainerEl.appendChild(cakeEl)
+  })
+}
+
+document
+  .getElementById('categorySelect')
+  .addEventListener('change', function (e) {
+    const selected = e.target.value
+    if (selected === 'all') {
+      renderItems(newArrivalData)
+    } else {
+      let filteredData = _.filter(newArrivalData, function (item) {
+        return item.category === selected
+      })
+      renderItems(filteredData)
+    }
+  })
+
+renderItems(newArrivalData)
